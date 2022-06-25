a4fa467d7dbdaa28d9d3144a1a918b33a77e1dc5
Jorge <jorgelarteagap@gmail.com>
2022-06-25 15:45:03 -0400

    [FEAT] skills

--- src/components/applications/Aplications.js
+++ src/components/applications/Aplications.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Titles } from '../utils/Titles'
 import './applications.css'
 import { Services } from './Services'
+import { Skills } from './Skills'
 
 export default function Application() {
   return (
@@ -9,6 +10,19 @@ export default function Application() {
       <div className="container">
         <Titles title="Aplicaciones" />
         <Services />
+      </div>
+      {/* Skills */}
+
+      <div className="skills">
+        <div className="skills__header is-flex is-align-items-center is-flex-direction-column">
+          <div className="skills__header-title has-text-weight-light is-size-3 is-size-5-mobile">
+            NUESTRAS HABILIDADES
+          </div>
+          <span className="skills__header-desc">
+            Contamos con experiencia y destreza en tecnologías
+          </span>
+          <Skills />
+        </div>
       </div>
     </section>
   )
---
+++ src/components/applications/Skills.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FaDatabase } from 'react-icons/fa'
+export const Skills = () => {
+  return (
+    <div className="skills__content py-5">
+      <div className="skills-items is-flex is-flex-direction-column is-justify-content-center is-align-items-center p-5">
+        <FaDatabase size={36} />
+        <p className="p-0 m-0 has-text-centered mt-3 has-text-weight-bold">
+          bases de datos
+        </p>
+      </div>
+      <div className="skills-items is-flex is-flex-direction-column is-justify-content-center is-align-items-center p-5">
+        <FaDatabase size={36} />
+        <p className="p-0 m-0 has-text-centered mt-3 has-text-weight-bold">
+          bases de datos
+        </p>
+      </div>
+      <div className="skills-items is-flex is-flex-direction-column is-justify-content-center is-align-items-center p-5">
+        <FaDatabase size={36} />
+        <p className="p-0 m-0 has-text-centered mt-3 has-text-weight-bold">
+          bases de datos
+        </p>
+      </div>
+      <div className="skills-items is-flex is-flex-direction-column is-justify-content-center is-align-items-center p-5">
+        <FaDatabase size={36} />
+        <p className="p-0 m-0 has-text-centered mt-3 has-text-weight-bold">
+          bases de datos
+        </p>
+      </div>
+    </div>
+  )
+}
